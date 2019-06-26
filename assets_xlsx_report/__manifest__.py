313334d666ca8a837c3aab53d238e534936be49c
GlobalItech <30366468+GlobalItech@users.noreply.github.com>
2019-06-26 10:04:29 +0500

    Add files via upload

---
+++ assets_xlsx_report/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Fixed Assets Register Report',
+    'category': 'Assets',
+    'license': "AGPL-3",
+    'summary': "Give The assets report in excel",
+    'author': 'Itech Resources',
+    'website': 'http://www.itechresources.net',
+    'depends': [
+                'base',
+                'account',
+                'account_asset',
+ #               'report_xlsx'
+                ],
+    'data': [
+            'wizard/report_menu.xml',
+            ],
+    'installable': True,
+    'auto_install': False,
+    'price': 30.00,
+    'currency': 'EUR',
+}
