0637117a99aef257b3b1ac708bc9b01aa324ed80
GlobalItech <30366468+GlobalItech@users.noreply.github.com>
2019-06-26 10:25:24 +0500

    Update __manifest__.py

--- assets_xlsx_report/__manifest__.py
+++ assets_xlsx_report/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Fixed Assets Register Report',
     'category': 'Assets',
-    'license': "LGPL-3",
+    'license': "AGPL-3",
     'version': '1.2.1',
     'summary': "Give The assets report in excel",
     'author': 'Itech Resources',
